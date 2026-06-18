e2623ab5984115c242166f78e39d4b951a4f9090
Jaya-student <mt1251570@maths.iitd.ac.in>
2026-06-18 10:34:12 +0530

    update complex_class.py

--- src/complex_class.py
+++ src/complex_class.py
@@ -1,1 +1,33 @@
+import math
+import numpy as np
 
+class Complex:
+    def __init__(self, real=0.0, imag=0.0):
+        self.real = float(real)
+        self.imag = float(imag)
+
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+
+    def __mul__(self, other):
+        r = self.real * other.real - self.imag * other.imag
+        i = self.real * other.imag + self.imag * other.real
+        return Complex(r, i)
+
+    def __truediv__(self, other):
+        denom = other.real**2 + other.imag**2
+        if denom == 0:
+            raise ZeroDivisionError()
+        r = (self.real * other.real + self.imag * other.imag) / denom
+        i = (self.imag * other.real - self.real * other.imag) / denom
+        return Complex(r, i)
+
+    def __abs__(self):
+        return math.sqrt(self.real**2 + self.imag**2)
+
+    def __str__(self):
+        sign = '+' if self.imag >= 0 else '-'
+        return f'{self.real:.10f} {sign} {abs(self.imag):.10f}j'
